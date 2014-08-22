45ad3170f645ea493aa9206ff0f670bbc32b964d
Nicole Yarroch <npyarroc@gmail.com>
2014-08-22 06:08:35 -0400

    Initial Commit

---
+++ GigilFaces/GigilFacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  GigilFacesViewController.h
+//  GigilFaces
+//
+//  Created by Nicole on 8/22/14.
+//  Copyright (c) 2014 nicole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GigilFacesViewController : UIViewController
+
+@end
