64b91bc8273c8ac94c22402f075003ab9e1c1e4a
Nicole Yarroch <npyarroc@gmail.com>
2014-12-14 20:17:49 -0500

    Fixed cancel button (was blurry when scaled)

---
+++ GigilFaces/TalkingMouthView.h
@@ -0,0 +1,13 @@
+//
+//  TalkingMouthView.h
+//  GigilFaces
+//
+//  Created by Nicole on 12/4/14.
+//  Copyright (c) 2014 nicole. All rights reserved.
+//
+
+#import "AnimatedImageView.h"
+
+@interface TalkingMouthView : AnimatedImageView
+
+@end
---
+++ GigilFaces/TalkingMouthView.m
@@ -0,0 +1,21 @@
+//
+//  TalkingMouthView.m
+//  GigilFaces
+//
+//  Created by Nicole on 12/4/14.
+//  Copyright (c) 2014 nicole. All rights reserved.
+//
+
+#import "TalkingMouthView.h"
+
+@implementation TalkingMouthView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
